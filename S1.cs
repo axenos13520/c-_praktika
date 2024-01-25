23a3bf4a32e0c99c04381fe61f560fb6f6460405
axenos13520 <152319486+axenos13520@users.noreply.github.com>
2024-01-25 20:32:02 +0600

    Add files via upload

---
+++ S1.cs
@@ -0,0 +1,48 @@
+﻿#define TASK13
+
+using System;
+
+namespace Test
+{
+    class S1
+    {
+        private static void main(string[] args)
+        {
+#if TASK1
+            float number = 0.01275f;
+            Console.WriteLine($"number = {Math.Round(number, 2)}");
+#elif TASK2
+            Console.WriteLine($"e = {Math.Round(Math.E, 1)}");
+#elif TASK3
+            Console.Write("Enter any number: ");
+            float number = float.Parse(Console.ReadLine().Replace('.', ','));
+            Console.WriteLine($"You entered {number}");
+#elif TASK4
+            Console.Write("Enter any number: ");
+            float number = float.Parse(Console.ReadLine().Replace('.', ','));
+            Console.WriteLine($"{number} is a number you entered");
+#elif TASK5
+            Console.WriteLine("1 13 49");
+#elif TASK6
+            Console.WriteLine("7  15  100");
+#elif TASK7
+            float num1 = 2, num2 = 7, num3 = 6;
+            Console.WriteLine($"{num1}  {num2}  {num3}");
+#elif TASK8
+            float num1 = 2, num2 = 7, num3 = 6, num4 = 12;
+            Console.WriteLine($"{num1} {num2} {num3} {num4}");
+#elif TASK9
+            Console.WriteLine("50\n10");
+#elif TASK10
+            Console.WriteLine("5\n10\n21");
+#elif TASK11
+            float num1 = 0.2f, num2 = 7, num3 = 62.93f, num4 = 19;
+            Console.WriteLine($"{num1}\n{num2}\n{num3}\n{num4}");\
+#elif TASK12
+            Console.WriteLine("5 10\n7 см");
+#elif TASK13
+            Console.WriteLine("2 кг\n13 17");
+#endif
+        }
+    }
+}
---
+++ S2_Part1.cs
@@ -0,0 +1,70 @@
+﻿#define TASK14A
+
+using System;
+
+namespace Test
+{
+    class S2_Part1
+    {
+        private static void main(string[] args)
+        {
+#if TASK1
+            // a
+            float x = 7;
+            Console.WriteLine($"y = {7*x*x+3*x+6}");
+            // b
+            float a = 5;
+            Console.WriteLine($"x = {12*a*a+7*a+12}");
+#elif TASK2
+            float a = 4;
+            Console.WriteLine($"P = {a*4}");
+#elif TASK3
+            float r = 6;
+            Console.WriteLine($"d = {r*2}");
+#elif TASK5
+            float a = 3;
+            Console.WriteLine($"V = {a*a*a}\nS = {a*a*6}");
+#elif TASK6
+            float r = 3;
+            Console.WriteLine($"l = {2*Math.PI*r}\nS = {Math.PI*r*r}");
+#elif TASK7
+            int num1 = 7, num2 = 5;
+            Console.WriteLine($"Average arith = {(num1 + num2) / 2f}\nAverage geom = {Math.Pow(num1*num2, 0.5f)}");
+#elif TASK8
+            float v = 5.4f, m = 10.8f;
+            Console.WriteLine($"p = {m / v}");
+#elif TASK9
+            float s = 10000f;
+            int n = 1000000;
+            Console.WriteLine($"p = {n / s}");
+#elif TASK10
+            float a = 7, b = 8;
+
+            if (a == 0)
+                throw new Exception("Incorrect \"A\" value!!!");
+
+            Console.WriteLine($"x = {-b/a}");
+#elif TASK11
+            float a = 3, b = 4;
+            Console.WriteLine($"c = {Math.Sqrt(a*a+b*b)}");
+#elif TASK12
+            float r = 3, R = 7;
+            Console.WriteLine($"S = {Math.PI*R*R-Math.PI*r*r}");
+#elif TASK13
+            float a = 3, b = 4;
+            Console.WriteLine($"P = {a+b+Math.Sqrt(a*a+b*b)}");
+#elif TASK13A
+            float num1 = 7, num2 = 5;
+            num1 = Math.Abs(num1);
+            num2 = Math.Abs(num2);
+            Console.WriteLine($"Average arith = {(num1 + num2) / 2f}\nAverage geom = {Math.Pow(num1 * num2, 0.5f)}");
+#elif TASK14
+            float a = 5, b = 3, h = 2;
+            Console.WriteLine($"P = {Math.Sqrt(Math.Pow((a-b)/2, 2)+h*h)*2+a+b}");
+#elif TASK14A
+            float a = 3, b = 4;
+            Console.WriteLine($"P = {(a+b)*2}\nd = {Math.Sqrt(a*a+b*b)}");
+#endif
+        }
+    }
+}
